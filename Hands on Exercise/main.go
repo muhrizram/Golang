5391933122431c96bf4e7c1ea9733f680b2d5eb9
muhrizram <muhrizram19@gmail.com>
2023-09-04 14:59:59 +0700

    Hands on Exercise

---
+++ Hands on Exercise/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("😍")
+	fmt.Println(`
+The most famous poem from Whitman’s celebrated Leaves of Grass, and selected by Jay Parini as the best American poem of all time. “Whitman reinvents American poetry in this peerless self-performance,” Parini writes, “finding cadences that seem utterly his own yet somehow keyed to the energy and rhythms of a young nation waking to its own voice and vision. He calls to every poet after him, such as Ezra Pound, who notes in “A Pact” that Whitman “broke the new wood.
+	`)
+}
